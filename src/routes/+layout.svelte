0513eac777c0e40644a0c7c03303399956a5ad30
MiguelBizzi <migueloliveirabizzi@gmail.com>
2024-08-14 21:45:07 -0300

    feat: add top header marketing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import '../app.css';
 </script>
 
+<section class="bg-red-500 py-1 text-center text-sm font-medium text-white">
+	<span>Eternize suas memórias! Compre já nossas fotos!</span>
+</section>
+
 <header class="relative flex h-[70px] w-full items-center justify-between bg-primary px-4">
 	<img src="/logo-header.png" alt="logo" class="w-[140px] object-fill object-center" />
 
